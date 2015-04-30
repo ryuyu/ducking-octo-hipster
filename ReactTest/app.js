da5679170047793f71e68b899fd01c5fb9c6a97b
Ryu Yu <x04ty29er@gmail.com>
2015-04-29 17:32:14 -0700

    Initial stuff

---
+++ ReactTest/app.js
@@ -0,0 +1,44 @@
+/// <reference path="react.d.ts" />
+var Timer = React.createClass({
+    render: function () {
+        return React.createElement("span", null, this.props.children.time.toUTCString());
+    }
+});
+var Greeter = React.createClass({
+    shouldComponentUpdate: function () {
+        if (this.state.needsRender) {
+            this.setState({
+                needsRender: false
+            });
+            return true;
+        }
+        return false;
+    },
+    updateTime: function () {
+        this.setState({
+            needsRender: true
+        });
+    },
+    getInitialState: function () {
+        return {
+            needRender: false
+        };
+    },
+    componentDidMount: function () {
+        setInterval(this.updateTime, this.props.timeInterval);
+    },
+    render: function () {
+        return React.createElement("div", null, "The time is: ", React.createElement(Timer, null, {
+            time: this.props.time
+        }));
+    }
+});
+window.onload = function () {
+    var container = document.createElement("div");
+    document.body.appendChild(container);
+    React.render(React.createElement(Greeter, {
+        time: new Date(),
+        timeInterval: 1000
+    }), container);
+};
+//# sourceMappingURL=app.js.map
---
+++ ReactTest/appjs.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="react.d.ts" />
+
+var Timer = React.createClass({
+    render: function () {
+        return <span>{this.props.time.toUTCString()}</span>;
+    }
+});
+
+var Greeter = React.createClass({
+    shouldComponentUpdate: function () {
+        if (this.state.needsRender) {
+            this.setState({
+                needsRender: false
+            });
+
+            this.props.time = new Date();
+            return true;
+        }
+
+        return false;
+    },
+    updateTime: function () {
+        this.setState({
+            needsRender: true
+        });
+    },
+    getInitialState: function () {
+        return { needsRender: false };
+    },
+    componentDidMount: function () {
+        setInterval(this.updateTime, this.props.timeInterval);
+    },
+    render: function () {
+        return <div>The time is: <Timer time={this.props.time} /></div>;
+    }
+});
+var timeObj = new Date();
+React.render(<Greeter time={new Date()} timeInterval="500" />, document.getElementById("content"));
